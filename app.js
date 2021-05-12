e0cfa07d29031d32414693a785d6ff5037b2de81
Saeed Bahatheq <saeed.bahatheq@gmail.com>
2021-05-12 01:59:41 -0400

    Add level 3 security: Hashing Authenticators

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5');
 
 const app = express();
 
@@ -24,11 +24,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password']
-})
 
 const User = new mongoose.model('User', userSchema);
 
@@ -50,7 +45,7 @@ app.post('/register', function(req, res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -64,7 +59,7 @@ app.post('/register', function(req, res) {
 
 app.post('/login', function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const hashedPassword = md5(req.body.password);
 
   User.findOne({
     email: username
@@ -73,7 +68,7 @@ app.post('/login', function(req, res) {
       console.log(err);
     }
     if (foundUser) {
-      if (foundUser.password === password) {
+      if (foundUser.password === hashedPassword) {
         res.render('secrets')
       }
     }
